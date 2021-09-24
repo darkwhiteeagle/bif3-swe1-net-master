49d7adf20aec54877ff71f4a50d8e4f32bbd79cb
darkwhiteeagle <tahasamaha@gmx.at>
2021-09-24 22:52:38 +0200

    new Shapes: Rectangle and Triangle

---
+++ bif3-swe1-net-master/Polymorphism/OptimizedSolution/Rectangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Bif3.Swe1.Oop.Polymorphism.OptimizedSolution
+{
+    public class Rectangle : Shape, IShapeComposition
+    {
+
+        private int _sideA ,_sideB;
+
+        public Rectangle(int x, int y, int a, int b) : base(x, y)
+        {
+            _sideA = a;
+            _sideB = b;
+        }
+
+        public double GetArea()
+        {
+            return _sideA * _sideB;
+        }
+
+        public double GetPerimeter()
+        {
+            return (2 *_sideA) + ( 2* _sideB);
+        }
+
+        public void PrintShapeType()
+        {
+            Console.WriteLine("I'm a Rectangle");
+        }
+    }
+}
---
+++ bif3-swe1-net-master/Polymorphism/OptimizedSolution/Triangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Bif3.Swe1.Oop.Polymorphism.OptimizedSolution
+{
+    public class Triangle : Shape, IShapeComposition
+    {
+
+        private int _sideA,_sideB,_sideC,_height;
+
+        public Triangle(int x, int y, int a, int b, int c ,int h) : base(x, y)
+        {
+            _sideA = a;
+            _sideB = b;
+            _sideC = c;
+            _height = h;
+        }
+
+        public double GetArea()
+        {
+            int g= Math.Max(Math.Max(_sideA, _sideB), _sideC);  //Grundseite berechnen
+
+            return (1/2) * g * _height;
+        }
+
+        public double GetPerimeter()
+        {
+            return _sideA + _sideB + _sideC;
+        }
+
+        public void PrintShapeType()
+        {
+            Console.WriteLine("I'm a Triangle");
+        }
+    }
+}
